36f6a0f47581ded1b187ea63aa0c5aa27c60acb2
=coolvishal005 <vishalgupta.vg316@gmail.com>
2020-12-29 14:51:38 +0530

    added maven project

---
+++ src/test/java/com/qa/tests/DemoTest2.java
@@ -0,0 +1,49 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	
+	@Test
+	public void Sum()
+	{
+		System.out.println("Sum");
+		int a=10;
+		int b=20;
+		
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void Sub()
+	{
+		System.out.println("Sub");
+		int a=10;
+		int b=20;
+		
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void Div()
+	{
+		System.out.println("Div");
+		int a=10;
+		int b=20;
+		
+		Assert.assertEquals(2, b/a);
+	}
+	
+	@Test
+	public void Mul()
+	{
+		System.out.println("Mul");
+		int a=10;
+		int b=20;
+		
+		Assert.assertEquals(200, a*b);
+	}
+}
+
+
